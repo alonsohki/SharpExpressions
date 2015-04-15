9e65a960e353ca0ca31ab1d28180d18d62c2b0db
Alberto Alonso <rydencillo@gmail.com>
2015-04-15 22:53:37 +0300

    Added a demo project to be able to run&test.

---
+++ Run/Program.cs
@@ -0,0 +1,37 @@
+﻿using Antlr.Runtime;
+using SharpExpressions.parser;
+using System;
+
+namespace SharpExpressions
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("> ");
+                string line = Console.ReadLine();
+
+                if (line == "exit")
+                {
+                    break;
+                }
+
+                try
+                {
+                    ANTLRStringStream stream = new ANTLRStringStream(line);
+                    SharpExpressionsLexer lexer = new SharpExpressionsLexer(stream);
+                    CommonTokenStream tokens = new CommonTokenStream(lexer);
+                    SharpExpressionsParser parser = new SharpExpressionsParser(tokens);
+                    parser.eval();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            while (true);
+        }
+    }
+}
